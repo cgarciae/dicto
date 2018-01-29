24032436cfe530fed76520c7b118b0923acbe3bb
Cristian Garcia <cgarcia.e88@gmail.com>
2018-01-28 18:10:54 -0800

    save

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "dicto",
-    version = "0.0.5",
+    version = "0.0.6",
     author = "Cristian Garcia",
     author_email = "cgarcia.e88@gmail.com",
     description = ("An object-like dictionary"),
